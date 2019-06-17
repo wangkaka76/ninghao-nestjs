6b3da128bde796b2b6a809239599578c6ea66cd3
王丽徽 <708982444@qq.com>
2019-06-17 13:41:00 +0800

    任务6：请求（Request）

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Req } from '@nestjs/common';
+
+@Controller('posts')
+export class PostsController {
+    @Get()
+    index(@Req() request) {
+        console.log(
+            request.ip,
+            request.hostname,
+            request.method,
+            )
+        return [
+            {
+                title:'hello ~'
+            }
+        ];
+    }
+}
