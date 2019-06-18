7941da7a2e644cab5290dd97740f1bb72a85911e
王丽徽 <708982444@qq.com>
2019-06-18 16:48:35 +0800

    任务_core02-13-using-exception-filters

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Req, Query, Headers, Param, Body, Post, HttpException, HttpStatus, ForbiddenException} from '@nestjs/common';
+import { Controller, Get, Req, Query, Headers, Param, Body, Post, HttpException, HttpStatus, ForbiddenException, UseFilters} from '@nestjs/common';
 import bodyParser = require('body-parser');
 import { CreatePostDto } from './posts.dto';
 import {DemoService} from './providers/demo/demo.service';
+import { DemoFilter } from '../../core/filters/demo.filter';
 
 @Controller('posts')
+// @UseFilters(DemoFilter)
 export class PostsController {
     constructor(private readonly demoService: DemoService) {}
 
@@ -19,8 +21,9 @@ export class PostsController {
     }
 
     @Post()
+    // @UseFilters(DemoFilter)
     Store(@Body() post: CreatePostDto) {
-        //throw new HttpException('没有权限！', HttpStatus.FORBIDDEN);
+        // throw new HttpException('没有权限！', HttpStatus.FORBIDDEN);
         throw new ForbiddenException('没有权限！');
         // this.demoService.create(post);
     }
