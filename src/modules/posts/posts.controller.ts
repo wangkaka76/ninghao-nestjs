d811699f59779d9f4e2ea2911202591b00712559
王丽徽 <708982444@qq.com>
2019-06-19 23:10:10 +0800

    任务core04-02-绑定拦截器bind-interceptors

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -1,14 +1,16 @@
-import { Controller, Get, Req, Query, Headers, Param, Body, Post, HttpException, HttpStatus, ForbiddenException, UseFilters, UsePipes, ValidationPipe, ParseIntPipe, UseGuards, SetMetadata} from '@nestjs/common';
+import { Controller, Get, Req, Query, Headers, Param, Body, Post, HttpException, HttpStatus, ForbiddenException, UseFilters, UsePipes, ValidationPipe, ParseIntPipe, UseGuards, SetMetadata, UseInterceptors} from '@nestjs/common';
 import bodyParser = require('body-parser');
 import { CreatePostDto } from './posts.dto';
 import {DemoService} from './providers/demo/demo.service';
 import { DemoFilter } from '../../core/filters/demo.filter';
 import { DemoAuthGuard } from '../../core/guards/demo-auth.guard';
 import { Roles } from '../../core/decorators/roles.decorator';
+import { LoggingInterceptor } from '../../../dist/core/interceptors/logging.interceptor';
 
 @Controller('posts')
 // @UseFilters(DemoFilter)
 // @UseGuards(DemoAuthGuard)
+@UseInterceptors(LoggingInterceptor)
 export class PostsController {
     constructor(private readonly demoService: DemoService) {}
 
