5718f042e9545d783832df2e22e5efa555018034
王丽徽 <708982444@qq.com>
2019-06-19 22:21:25 +0800

    core03-12-custom-decorators

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -4,6 +4,7 @@ import { CreatePostDto } from './posts.dto';
 import {DemoService} from './providers/demo/demo.service';
 import { DemoFilter } from '../../core/filters/demo.filter';
 import { DemoAuthGuard } from '../../core/guards/demo-auth.guard';
+import { Roles } from '../../core/decorators/roles.decorator';
 
 @Controller('posts')
 // @UseFilters(DemoFilter)
@@ -27,8 +28,9 @@ export class PostsController {
     @Post()
     // @UseFilters(DemoFilter)
     @UsePipes(ValidationPipe)
-    @SetMetadata('roles', ['member'])
+    // @SetMetadata('roles', ['member'])
     // @UseGuards(DemoAuthGuard)
+    @Roles('member')
     Store(@Body() post: CreatePostDto) {
         // throw new HttpException('没有权限！', HttpStatus.FORBIDDEN);
         // throw new ForbiddenException('没有权限！');
