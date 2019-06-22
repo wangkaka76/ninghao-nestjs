6964fd1424f850a25d43d3d0ceab56bba6adff7f
王丽徽 <708982444@qq.com>
2019-06-22 23:37:32 +0800

    任务database05-05-post-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostModule } from './modules/post/post.module';
 
 @Module({
   imports: [
@@ -14,7 +15,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       database: 'nest',
       synchronize: true,
       entities: [__dirname + '/**/*.entity.ts']
-    })
+    }),
+    PostModule
   ],
   controllers: [AppController],
   providers: [AppService],
