b18f70a46735d425b5811ca1535606dcccc93cec
王丽徽 <708982444@qq.com>
2019-07-05 17:34:23 +0800

    任务users07-08-show-user

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { UserDto } from './user.dto';
+import { async } from 'rxjs/internal/scheduler/async';
 
 @Injectable()
 export class UserService {
@@ -21,6 +22,16 @@ export class UserService {
 
         const entity = await this.userRepository.create(data);
         await this.userRepository.save(entity);
+        return entity;
+    }
+    
+    async show(id: string) {
+        const entity = await this.userRepository.findOne(id);
+
+        if (!entity) {
+            throw new NotFoundException('没找到用户。');
+        }
+
         return entity;
     }
 }
