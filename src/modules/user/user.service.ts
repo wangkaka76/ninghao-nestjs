0d509ac9c18021fd9d68e316ba80862137e83091
王丽徽 <708982444@qq.com>
2019-07-06 09:51:25 +0800

    合并user分支提交03

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from './user.entity';
+import { User } from './user.entity'; 
 import { Repository } from 'typeorm';
 import { UserDto } from './user.dto';
 import { async } from 'rxjs/internal/scheduler/async';
