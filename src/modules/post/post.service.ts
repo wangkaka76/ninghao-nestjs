bbe2cba0854bc07c1f9d308bd8eb53ce9bad5f01
王丽徽 <708982444@qq.com>
2019-08-02 23:23:37 +0800

    任务：pagination-sort13-08-query-string-orderby

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -62,7 +62,7 @@ export class PostService {
   }
 
   async index(options: ListOptionsInterface) {
-    const { categories, tags, page, limit } = options;
+    const { categories, tags, page, limit, sort, order } = options;
     const queryBuilder = await this.postRepository
       .createQueryBuilder('post');
 
@@ -84,7 +84,7 @@ export class PostService {
 
     queryBuilder
       .orderBy({
-        'post.created': 'DESC'
+        [`post.${sort}`]: order
       })
 
 
