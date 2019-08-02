cbbc3763d88ca58a74817f0b1d33b8328102dafc
王丽徽 <708982444@qq.com>
2019-08-02 09:17:22 +0800

    任务：统计放头部pagination-sort13-06-transform-interceptor

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -7,6 +7,7 @@ import { User as UserEntity } from '../user/user.entity';
 import { async } from 'rxjs/internal/scheduler/async';
 import { ListOptions } from '../../core/decorators/list-options.decorator';
 import { ListOptionsInterface } from '../../core/interfaces/list-options.interface';
+import { TransformInterceptor } from '../../core/interceptors/transform.interceptor';
 
 @Controller('posts')
 export class PostController {
@@ -21,7 +22,7 @@ export class PostController {
   }
 
   @Get()
-  @UseInterceptors(ClassSerializerInterceptor)
+  @UseInterceptors(ClassSerializerInterceptor, TransformInterceptor)
   async index(
     @ListOptions({ limit: 2 }) Options: ListOptionsInterface
   ) {
